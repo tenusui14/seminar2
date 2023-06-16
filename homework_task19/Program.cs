896d954b0ae4a2ec36f1b577d7051f1bd502c9d5
Artem Tsytsylin <workhardgofock@gmail.com>
2023-06-16 22:36:34 +0300

    homework task19 is done

---
+++ homework_task19/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 19: Напишите программу, которая принимает 
+// на вход пятизначное число и проверяет, является ли 
+// оно палиндромом. (Зеркальным числом 12321)
+
+
+Console.WriteLine("Введите пятизначное число: ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+int numForReverse = num;
+int reverse = 0;
+if (num < 100000){
+    while (numForReverse > 0){
+        reverse = reverse*10 + numForReverse%10;
+        numForReverse = numForReverse / 10;
+    }
+    if (reverse == num) {Console.WriteLine($"Число {num} ЯВЛЯЕТСЯ палиндромом");}
+    else {Console.WriteLine($"Число {num} НЕ является палиндромом");}
+}
+else Console.WriteLine($"ОШИБКА. Введенное число {num} не является пятизначным");
