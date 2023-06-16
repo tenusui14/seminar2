53e46d907c32e5b4b8a067b0593a51943a9bd8d2
Artem Tsytsylin <workhardgofock@gmail.com>
2023-06-16 20:41:22 +0300

    task17 is done

---
+++ task6(xz)/Program.cs
@@ -0,0 +1,12 @@
+﻿// 
+Console.WriteLine("Введите координату по оси х: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату по оси у: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > 0 && y > 0) Console.WriteLine(1);
+else if (x < 0 && y > 0) Console.WriteLine(2);
+else if ( x < 0 && y < 0) Console.WriteLine(3);
+else if ( x > 0 && y < 0) Console.WriteLine(4);
+else Console.WriteLine("Невозможно определить координатную четверть");
